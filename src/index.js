2aa9bf258a47b80277935c2b0e8537b99f1fc776
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-11 05:01:29 +0300

    Add home page placeholder.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./styles.css";
+// Used images
+//  https://unsplash.com/photos/photo-of-pub-set-in-room-during-daytime-poI7DelFiVA
 
-const renderDOM = (function () {
+const renderBar = (function () {
   const body = document.querySelector("body");
   const footer = () => {
     const footerContainer = document.createElement("footer");
     const date = document.createElement("p");
-    date.innerHTML = `Copyright ${new Date().getFullYear()}`;
+    date.innerHTML = `Copyright © ${new Date().getFullYear()}`;
     footerContainer.appendChild(date);
     body.appendChild(footerContainer);
   };
@@ -13,7 +15,6 @@ const renderDOM = (function () {
   return { footer };
 })();
 
-
-renderDOM.footer();
+renderBar.footer();
 
 console.log("The server is running");
