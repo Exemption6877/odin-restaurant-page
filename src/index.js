7b5d9a30e5b4bae0c53e0e7d56227c88142948c2
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-11 06:46:10 +0300

    Render about page on button click.

--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ const renderBar = (function () {
   return { footer };
 })();
 
+const resetPage = () => {
+  const content = document.querySelector("#content");
+  if (content.childElementCount > 0) {
+    content.replaceChildren();
+  }
+};
+
 const renderPage = (function () {
   const content = document.querySelector("#content");
   const main = () => {
@@ -34,8 +41,44 @@ const renderPage = (function () {
     home.append(heading, image, paragraph);
     content.appendChild(home);
   };
-  return { main };
+
+  const about = () => {
+    const about = document.createElement("div");
+    about.classList.add("home");
+    const heading = document.createElement("h1");
+    heading.textContent = "About Us";
+    const paragraph = document.createElement("p");
+    paragraph.textContent = `Welcome to The Pizza Place, where passion meets flavor!
+    We have been serving handcrafted, oven-fresh pizzas made from the finest ingredients for over a decade.
+    From our crispy thin crusts to our rich, savory sauces, every bite is made with love.
+    At The Pizza Place, we believe in more than just great food—we believe in creating unforgettable experiences. 
+    Whether you are dining in with family, grabbing a quick slice,
+    or ordering for a party, we promise quality, taste, and service that will keep you coming back.`;
+    const address = document.createElement("h3");
+    address.textContent =
+      "📍 Location: 1234 Flavor Street, Pizza Town, PT 56789";
+    const phone = document.createElement("h3");
+    phone.textContent = "📞 Phone: (555) 123-4567";
+    about.append(heading, paragraph, address, phone);
+    content.appendChild(about);
+  };
+
+  return { main, about };
 })();
+
+const navButtons = document.querySelectorAll("button");
+
+navButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (event.target.value == "about") {
+      resetPage();
+      renderPage.about();
+    } else if (event.target.value == "home") {
+      resetPage();
+      renderPage.main();
+    }
+  });
+});
 
 renderPage.main();
 renderBar.footer();
